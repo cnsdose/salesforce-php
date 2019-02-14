d30aa3b0cca7227c87b26476d23af35f8f3e2fd8
Frederick Zhang <frederick888@tsundere.moe>
2019-02-14 17:29:43 +1100

    salesforce oauth jwt flow

--- config/salesforce.php
+++ config/salesforce.php
@@ -9,11 +9,16 @@ return [
     'authentication_url' => 'https://login.salesforce.com/services/oauth2/token',
     'api_version' => 'v44.0',
 
+    'auth_flow' => 'jwt',                                        // jwt or username_password
+
     'username' => env('SALESFORCE_USERNAME'),
     'password' => env('SALESFORCE_PASSWORD'),
     'security_token' => env('SALESFORCE_SECURITY_TOKEN'),
     'client_id' => env('SALESFORCE_CLIENT_ID'),
     'client_secret' => env('SALESFORCE_CLIENT_SECRET'),
+
+    'public_key' => env('SALESFORCE_PUBLIC_KEY'),           // key file path or content directly, e.g. file_get_contents(path)
+    'private_key' => env('SALESFORCE_PRIVATE_KEY'),
 
     'custom_fields' => [
         /*
